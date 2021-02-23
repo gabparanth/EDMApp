a8d4b14e96007f47b5b7d792728f43eaf3a41549
Gabriel Paranthoen <gabrielparanthoen@Bolt-2.home>
2021-02-23 13:20:46 +0100

    test auth

--- hosting/files/app.js
+++ hosting/files/app.js
@@ -2,37 +2,47 @@ const STITCH_APP_ID = 'edmapp-cyexg';
 const client = stitch.Stitch.initializeDefaultAppClient(STITCH_APP_ID);
 
 
+// function checkAuth(callback) {
+//     if (!client.auth.isLoggedIn) {
+//         if (client.auth.hasRedirectResult()) {
+//             client.auth.handleRedirectResult().then(user => {
+//                 callback(true);
+//             });
+//         } else {
+//             const credential = new stitch.GoogleRedirectCredential();
+//             client.auth.loginWithRedirect(credential);
+//         }
+//     } else {
+//         callback(true);
+//     }
+
+// }
+
 function checkAuth(callback) {
-    if (!client.auth.isLoggedIn) {
-        if (client.auth.hasRedirectResult()) {
-            client.auth.handleRedirectResult().then(user => {
-                callback(true);
-            });
-        } else {
-            const credential = new stitch.GoogleRedirectCredential();
-            client.auth.loginWithRedirect(credential);
-        }
-    } else {
+    client.auth
+      .loginWithCredential(new stitch.AnonymousCredential())
+      .then((user) => {
         callback(true);
-    }
+      });
+  }
 
-}
-
-function TestCheckAuth(){
-    checkAuth(function (isAuthenticated) {
-        client.callFunction('toto', []).then((result) => {
-            callback(result);
-            console.log(result);
-        });
-    });
-}
-
-
-TestCheckAuth();
-
-// function getFiles(callback) {
-//     client.callFunction('toto', []).then((files) => {
-//         console.log(files);
-//         callback(files);
+// function TestCheckAuth(){
+//     checkAuth(function (isAuthenticated) {
+//         client.callFunction('toto', []).then((result) => {
+//             callback(result);
+//             console.log(result);
+//         });
 //     });
 // }
+
+
+// TestCheckAuth();
+
+function getFiles(callback) {
+    checkAuth(function (isAuthenticated) {
+    client.callFunction('toto', []).then((files) => {
+        console.log(files);
+        callback(files);
+    });
+});
+}
