c4e62f77082788a0229ee67be4d0ed8f6659f074
Gabriel Paranthoen <gabrielparanthoen@Bolt-2.home>
2021-02-23 13:52:36 +0100

    test auth

--- hosting/files/app.js
+++ hosting/files/app.js
@@ -19,7 +19,8 @@ function checkAuth(callback) {
 
 }
 
-// auth anonymously
+
+// auth anonymously for testing only 
 
 // function checkAuth(callback) {
 //     client.auth
@@ -38,3 +39,67 @@ function getFiles(callback) {
     });
 });
 }
+
+function printList(files) {
+
+
+
+    var table = document.getElementById('fileList');
+
+    table.innerHTML = `<tr class=table-body>
+    <td>
+      File Name
+    </td>
+    <td>
+      File owner
+    </td>
+    <td>
+    Status
+    </td>
+    <td>
+    Edit configuration
+    </td>
+    <td>s
+              
+    </td>
+  </tr>`;
+
+    files.forEach((file) => {
+        var row = document.createElement('tr');
+        row.setAttribute('class', 'table-body');
+        // var tblUser = document.createElement('td');
+        var tblName = document.createElement('td');
+        var tblOwner = document.createElement('td');
+        // var tblDelete = document.createElement('td');
+        // var tblWhitelisted = document.createElement('td');
+
+        // var tblStatus = document.createElement('td');
+        // var tblEditconfiguration = document.createElement('td');
+
+        // tblUser.innerHTML = cluster.email;
+        tblName.innerHTML = file.fileName;
+        tblOwner.innerHTML = file.owner;
+        // tblStatus.innerHTML = cluster.whitelistingPolicy;
+        // tblWhitelisted.innerHTML = cluster.pausedCluster;
+
+        // var editConfigurationButton = document.createElement('button');
+        // editConfigurationButton.innerHTML = "..."
+        // editConfigurationButton.setAttribute('onclick', `editConfiguration('${cluster._id}','${cluster.project_name}','${cluster.cluster_name}','${cluster.whitelistingPolicy}')`);
+        // editConfigurationButton.setAttribute('class', 'three-dot-button');
+        // tblEditconfiguration.appendChild(editConfigurationButton);
+
+        // row.appendChild(tblUser);
+        row.appendChild(tblName);
+        row.appendChild(tblOwner);
+        // row.appendChild(tblStatus);
+        // row.appendChild(tblEditconfiguration);
+
+        table.appendChild(row);
+    });
+}
+
+
+
+getFiles(function (files) {
+    printList(files);
+});
