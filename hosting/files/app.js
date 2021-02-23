ba1ad6d303cc1d2452eb159255e2d6236c5a3b53
Gabriel Paranthoen <gabrielparanthoen@Bolt-2.home>
2021-02-23 12:25:20 +0100

    test auth

--- hosting/files/app.js
+++ hosting/files/app.js
@@ -2,3 +2,29 @@ const STITCH_APP_ID = 'edmapp-cyexg'
 
 
 const client = stitch.Stitch.initializeDefaultAppClient(STITCH_APP_ID);
+
+
+function checkAuth(callback) {
+    if (!client.auth.isLoggedIn) {
+        if (client.auth.hasRedirectResult()) {
+            client.auth.handleRedirectResult().then(user => {
+                callback(true);
+            });
+        } else {
+            const credential = new stitch.GoogleRedirectCredential();
+            client.auth.loginWithRedirect(credential);
+        }
+    } else {
+        callback(true)
+    }
+
+}
+
+function TestCheckAuth(){
+    checkAuth(function (isAuthenticated) {
+        client.callFunction('toto', []).then((result) => {
+            callback(result);
+            console.log(result);
+        });
+    });
+}
