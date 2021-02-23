6a481747a4560386bed1f159fc1bab558eb0f3de
Gabriel Paranthoen <gabrielparanthoen@Bolt-2.home>
2021-02-23 12:11:24 +0100

    first function

---
+++ functions/toto/source.js
@@ -0,0 +1,16 @@
+exports = async function()
+{
+  const collection = context.services.get("mongodb-atlas").db("EDMApp").collection("Test");
+  
+  // var emailAddress = context.user.data.email.split("@");
+  
+  
+  // var emailAddressMongo = emailAddress[0] + "@mongodb.com";
+  
+  // console.log(emailAddressMongo);
+  
+  var result = collection.find({}).toArray();
+  
+  
+return result;
+};
