01e942e9fcbebfdedb7f7dcb3bc2a121e4c9900d
Gabriel Paranthoen <gabrielparanthoen@Bolt-2.home>
2021-02-23 16:32:43 +0100

    impl S3

--- functions/uploadFileToS3/source.js
+++ functions/uploadFileToS3/source.js
@@ -2,7 +2,7 @@ exports = function(base64EncodedImage, fileName) {
     // Convert the base64 encoded image string to a BSON Binary object
     const binaryImageData = BSON.Binary.fromBase64(base64EncodedImage, 0);
     // Instantiate an S3 service client
-    const s3Service = context.services.get('S3').s3('eu-west-1');
+    const s3Service = context.services.get('aws').s3('eu-west-1');
     // Put the object to S3
     return s3Service.PutObject({
       'Bucket': 'edmapp',
